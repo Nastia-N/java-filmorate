65c5cc7e291913c7054fcf94d3fdc39c131215f0
Анастасия Нагайцева <nagaitzeva.anastasia@yandex.ru>
2025-09-12 21:14:14 +0300

    Доработка моделей: жанр и рейтинг MPA для фильмов, заяаки в друзья для юзеров.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,7 +6,9 @@ import lombok.EqualsAndHashCode;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,7 +16,7 @@ import java.util.Set;
 public class User extends NewUser {
     @Min(value = 1, message = "Id должен быть положительным числом")
     private long id;
-    private Set<Long> friends = new HashSet<>();
+    private Map<Long, Friendship> friends = new HashMap<>();
     private Set<Long> likedFilms = new HashSet<>();
 
     public User(long id, String email, String login, String name, LocalDate birthday) {
@@ -22,14 +24,34 @@ public class User extends NewUser {
         this.id = id;
     }
 
-    public void addFriend(Long id) {
-        if (id != null && id > 0) {
-            if (this.id == id) {
+    public void sendFriendRequest(Long friendId) {
+        if (friendId != null && friendId > 0) {
+            if (this.id == friendId) {
                 throw new ValidationException("Пользователь не может добавить себя в друзья");
             }
-            friends.add(id);
+            friends.put(friendId, Friendship.SENDING);
         } else {
             throw new ValidationException("Id должен быть положительным числом");
+        }
+    }
+
+    public void waitFriendRequest(Long friendId) {
+        if (friendId != null && friendId > 0) {
+            friends.put(friendId, Friendship.WAITING);
+        } else {
+            throw new ValidationException("Id должен быть положительным числом");
+        }
+    }
+
+    public void confirmFriend(Long friendId) {
+        if (friends.containsKey(friendId) && friends.get(friendId) == Friendship.WAITING) {
+            friends.put(friendId, Friendship.CONFIRMED);
+        }
+    }
+
+    public void updateFriend(Long id) {
+        if (friends.containsKey(id) && friends.get(id) == Friendship.SENDING) {
+            friends.put(id, Friendship.CONFIRMED);
         }
     }
 
